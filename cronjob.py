6527c341769be886a14829209b402bab08988244
Doron Chen <cdoron@il.ibm.com>
2023-03-06 11:42:07 +0200

    check statuses
    Signed-off-by: Doron Chen <cdoron@il.ibm.com>

--- cronjob.py
+++ cronjob.py
@@ -1,9 +1,44 @@
 import json
+import time
 
 from kubernetes import config, dynamic
-from kubernetes.client import api_client
+from kubernetes.client import api_client, ApiException
 from kubernetes import client as k8s_client
 
+
+def wait_for_fybrikapplication_to_be_ready(custom_object_api):
+    while True:
+        try:
+            fybrikapplication = custom_object_api.get_namespaced_custom_object(
+                group="app.fybrik.io", version="v1beta1",
+                namespace="fybrik-airbyte-sample", plural="fybrikapplications",
+                name="my-app"
+            )
+        except ApiException:
+            print("FybrikApplication not found")
+            time.sleep(1)
+            continue
+
+        if not fybrikapplication["status"]["ready"]:
+            print("FybrikApplication not ready")
+            time.sleep(1)
+            continue
+
+        assetsReady = True
+        for name, asset in fybrikapplication["status"]["assetStates"].items():
+            for condition in asset["conditions"]:
+                if condition["type"] == "Ready":
+                    if condition["status"] != 'True':
+                        print("asset not ready")
+                        assetsReady = False
+                        break
+
+        if not assetsReady:
+            time.sleep(1)
+            continue
+
+        print(str(json.dumps(fybrikapplication)))
+        return
 
 def main():
     dynamic.DynamicClient(
@@ -12,13 +47,7 @@ def main():
 
     custom_object_api = k8s_client.CustomObjectsApi()
 
-    fybrikapplication = custom_object_api.get_namespaced_custom_object(
-        group="app.fybrik.io", version="v1beta1",
-        namespace="fybrik-airbyte-sample", plural="fybrikapplications",
-        name="my-app"
-    )
-
-    print(str(json.dumps(fybrikapplication)))
+    wait_for_fybrikapplication_to_be_ready(custom_object_api)
 
 
 if __name__ == "__main__":

65d72f919bcb318431f4c417b7fb47cb9ff7fc97
Doron Chen <cdoron@il.ibm.com>
2023-03-19 20:29:50 +0200

    minor change
    Signed-off-by: Doron Chen <cdoron@il.ibm.com>

--- workload.py
+++ workload.py
@@ -19,6 +19,7 @@ def run_workload(read_asset_name, write_asset_name, endpoints):
     info = client.get_flight_info(fl.FlightDescriptor.for_command(json.dumps(request)))
     reader: fl.FlightStreamReader = client.do_get(info.endpoints[0].ticket)
     table: pa.Table = reader.read_all()
+    print(table.to_pandas())
 
     print("Now let us try to write")
     endpoint = endpoints[write_asset_name]
@@ -26,7 +27,7 @@ def run_workload(read_asset_name, write_asset_name, endpoints):
 
     request = {
         "asset": write_asset_name,
-        "json_schema": '{"$schema": "http://json-schema.org/draft-07/schema#", "type": "object",  "properties": {"step": { "type": ["null", "string"] }, "type": { "type": ["null", "string"] }, "amount": { "type": ["null", "string"] }, "nameOrig": { "type": ["null", "string"] }, "oldbalanceOrg": { "type": ["null", "string"] }, "newbalanceOrig": { "type": ["null", "string"] }, "nameDest": { "type": ["null", "string"] }, "oldbalanceDest": { "type": ["null", "string"] }, "newbalanceDest": { "type": ["null", "string"] }, "isFraud": { "type": ["null", "string"] }, "isFlaggedFraud": { "type": ["null", "string"] } }}',
+        "json_schema": '{"$schema": "http://json-schema.org/draft-07/schema#", "type": "object",  "properties": {"step": { "type": ["null", "string"] }, "type": { "type": ["null", "string"] }, "amount": { "type": ["null", "string"] }, "nameOrig": { "type": ["null", "string"] }, "oldbalanceOrg": { "type": ["null", "string"] }, "newbalanceOrig": { "type": ["null", "string"] }, "nameDest": { "type": ["null", "string"] }, "oldbalanceDest": { "type": ["null", "string"] }, "newbalanceDest": { "type": ["null", "string"] }, "isFraud": { "type": ["null", "string"] }, "isFlaggedFraud": { "type": ["null", "string"] }, "copied_column": { "type": ["null", "string"] } }}',
     }
 
     writer, _ = client.do_put(fl.FlightDescriptor.for_command(json.dumps(request)),
